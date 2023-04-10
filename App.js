6e6bda2cf9b0720f4ca80a59bf36121bf2e20e9c
aasoto <andresalfredosotosuarez@gmail.com>
2023-04-09 21:47:44 -0500

    feat: Install NativeWind and creates elements text, images and buttons

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-slate-800 gap-5">
+      <Text className="text-white text-xl">Primera aplicación</Text>
+
+      {/* Imagen tomada de internet */}
+      <Image 
+        source={{uri: 'https://picsum.photos/200/200'}}
+        className="w-40 h-40 rounded-full"
+      />
+
+      {/* Imagen local */}
+      <Image
+        source={require('./assets/images/andres.jpg')}
+        className="w-40 h-40 rounded-full"
+      />
+
+      {/* Botón predeterminado */}
+      <Button
+        onPress={() => Alert.alert('Alerta botón predeterminado')}
+        title="Botón predeterminado"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      {/* Botón personalizable */}
+      <TouchableOpacity
+        onPress={() => Alert.alert('Alerta botón personalizado')}
+        className="bg-blue-600 px-5 py-2 rounded-md shadow"
+      >
+        <Text className="text-white capitalize text-lg">Botón personalizable</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
